78be8515f34978c6be81ba85c20f978147736b70
Jack <jackleighton@live.co.uk>
2021-02-22 13:26:39 +0100

    Accept user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
 #put students into an array
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name:"Darth Vader", cohort: :november},
- {name:"Nurse Ratched", cohort: :november},
- {name:"Michael Corleone", cohort: :november},
- {name:"Alex DeLarge", cohort: :november},
- {name:"The Wicked Witch of the West", cohort: :november},
- {name:"Terminator", cohort: :november},
- {name:"Freddy Krueger", cohort: :november},
- {name:"The Joker", cohort: :november},
- {name:"Joffrey Baratheon", cohort: :november},
- {name:"Norman Bates", cohort: :november}
-]
+# students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name:"Darth Vader", cohort: :november},
+#  {name:"Nurse Ratched", cohort: :november},
+#  {name:"Michael Corleone", cohort: :november},
+#  {name:"Alex DeLarge", cohort: :november},
+#  {name:"The Wicked Witch of the West", cohort: :november},
+#  {name:"Terminator", cohort: :november},
+#  {name:"Freddy Krueger", cohort: :november},
+#  {name:"The Joker", cohort: :november},
+#  {name:"Joffrey Baratheon", cohort: :november},
+#  {name:"Norman Bates", cohort: :november}
+# ]
+
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add student hash to the array
+    students << {name: name, cohort: :novemeber}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villians Academy"
@@ -26,6 +43,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
