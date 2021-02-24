c3e2936c12e6b61a0923385c633e56e45196667a
Jack <jackleighton@live.co.uk>
2021-02-24 14:07:24 +0100

    Correct errors and centre user input prompts

--- copy_directory.rb
+++ copy_directory.rb
@@ -1,14 +1,16 @@
+#create instance variable width that inputs the width I want centre to be
+@width = 55
 
 #request input from user to add a new student
 def create_directory
-  puts "Input a new student? 'YES' or 'NO'"
+  puts "Input a new student? 'YES' or 'NO'".center(@width)
   user_input = gets.chomp.upcase
   if user_input == "YES"
     return true
   elsif user_input == "NO"
     return false
   else
-    "Did not understand input"
+    "Did not understand input".center(@width)
   end
   #returns method to start again
   create_directory
@@ -22,16 +24,16 @@ def input_students
 
   while begin_directory
     #grabs user input for name and country, height
-    puts "Please enter the name of the students"
+    puts "Please enter the name of the students".center(@width)
     name = gets.chomp
-    puts "Please enter country of birth"
+    puts "Please enter country of birth".center(@width)
     country_of_birth = gets.chomp
-    puts "Please enter height (if available)"
+    puts "Please enter height in cm (if available)".center(@width)
     height = gets.chomp
     #grab user input for hobbies and stores it in an empty array. Join used to convert into string
-    hobbies = [].join(" ")
-    puts "Please enter student's hobbies"
-    puts "(To finish, just hit return twice)"
+    hobbies = []
+    puts "Please enter student's hobbies".center(@width)
+    puts "(To finish, just hit return twice)".center(@width)
     hobby = gets.chomp
     while !hobby.empty?
       hobbies << hobby
@@ -39,9 +41,9 @@ def input_students
     end
     hobbies
     #pushes name, country, cohort and hobbies onto students array
-    students << {name: name, country_of_birth: country_of_birth, height: height, hobbies: hobbies, cohort: :november}
+    students << {name: name, country_of_birth: country_of_birth, height: height, hobbies: hobbies.join(", "), cohort: :november}
     #counts number of students
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(@width)
     #creates a variable that triggers create_directory to give user option to end
     begin_directory = create_directory
   end
@@ -49,8 +51,8 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villians Academy"
-  puts "-------------"
+  puts "The students of Villians Academy".center(@width)
+  puts "-------------".center(@width)
 end
 
 #method to count number of students and puts name, country...
@@ -64,7 +66,7 @@ end
 
 #method to output overall total of students
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(@width)
 end
 
 students = input_students
