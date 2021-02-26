bee38a823c845a1ab5b186d4cce52464fd5bd1f1
Jack <jackleighton@live.co.uk>
2021-02-26 13:33:44 +0100

    Ammend code to avoid DRY (re-commit)

--- copy_directory.rb
+++ copy_directory.rb
@@ -124,8 +124,6 @@ def add_students(name, country_of_birth, height, hobbies, cohort)
   @students << {name: name, country_of_birth: country_of_birth, height: height, hobbies: hobbies, cohort: cohort}
 end
 
-#test
-
 def print_header
   if !@students.empty?
     puts "The students of Villians Academy".center(@width)
